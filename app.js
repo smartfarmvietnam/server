2bfca0801cb2957dceb4e6150c86a3a5694f6e18
WonBeomChoi <cwb2310@gmail.com>
2019-08-20 13:10:25 +0700

    끝?

--- app.js
+++ app.js
@@ -57,110 +57,41 @@ console.log("con!");
  * 클라이언트와 http통신하는 곳
  */
 
-//스마트팜 대기습도
-app.get('/smart_humid', (req,res) => {
-  humid = req.query.humid;
-  humid_date = req.query.humid_date;
-  if(humid  && humid_date){
-    console.log(req.query);
-    
-    console.log('humid : '+ humid);
-    console.log('humid_date : '+ humid_date);
-    connection.query('insert into smartfarm_humid (humid, date) values(?,?)',  
-    [humid,humid_date],
-    (err, rows) => {
-        if(err) console.log(err);
-    })
-    res.send("ok")
-  }
-  else res.send("fail");
-});
-
-app.get('/smart_humid_temp_A', (req, res) => {
-  var date = get_time();
-  humid_date = date  + req.query.humid_date;
-  humid = req.query.humid;
-  temp = req.query.temp;
-
-  if(humid && temp && humid_date){
-    console.log(req.query);
-    
-    console.log('humid : '+ humid);
-    console.log('humid_date : '+ temp);
-    console.log('humid_date : '+ humid_date);
-    connection.query('insert into smartfarm_humid_A (humid, date) values(?,?)',  
-    [humid, humid_date],
-    (err, rows) => {
-        if(err) console.log(err);
-    })
-    connection.query('insert into smartfarm_temp_A (temp, date) values(?,?)',  
-    [temp, humid_date],
-    (err, rows) => {
-        if(err) console.log(err);
-    })
-    res.send("ok")
-  }
-  else res.send("fail");
-});
-app.get('/aqua_fish_data',(req,res)=>{
-  var date = get_time();
-  var aqua_date = date + req.query.date;
-  var aqua_ec = req.query.ec;
-  var aqua_do = req.query.do;
-  var aqua_temp = req.query.wtemp;
-  var aqua_ph = req.query.ph;
-  connection.query('insert into aqua_fish_ec (ec, date) values(?,?)',
-  [aqua_ec, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-  connection.query('insert into aqua_fish_wtemp (wtemp, date) values(?,?)',  
-  [aqua_temp, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-  connection.query('insert into aqua_fish_do (do, date) values(?,?)',
-  [aqua_do, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-  connection.query('insert into aqua_fish_ph (ph, date) values(?,?)',  
-  [aqua_ph, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-})
+//박테리아통 데이터넣기
 app.get('/aqua_bio_data',(req,res)=>{
   var date = get_time();
   var aqua_date = date + req.query.date;
   var aqua_ec = req.query.ec;
   var aqua_do = req.query.do;
-  var aqua_temp = req.query.wtemp;
+  var aqua_wtemp = req.query.wtemp;
   var aqua_ph = req.query.ph;
-  connection.query('insert into aqua_bio_ec (ec, date) values(?,?)',
-  [aqua_ec, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-  connection.query('insert into aqua_bio_wtemp (wtemp, date) values(?,?)',  
-  [aqua_temp, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-  connection.query('insert into aqua_bio_do (do, date) values(?,?)',
-  [aqua_do, aqua_date],
-  (err, rows) => {
-      if(err) console.log(err);
-  })
-  connection.query('insert into aqua_bio_ph (ph, date) values(?,?)',  
-  [aqua_ph, aqua_date],
+  connection.query('insert into aqua_bio (ph, do, wtemp, ec, date) values(?,?,?,?,?)',
+  [aqua_ph,aqua_do,aqua_ph,aqua_wtemp,aqua_ec, aqua_date],
   (err, rows) => {
       if(err) console.log(err);
   })
 })
-app.get('/smart_humid_temp_B', (req, res) => {
+
+//물고기통 데이터넣기
+app.get('/aqua_fish_data',(req,res)=>{
   var date = get_time();
-  humid_date = date  + req.query.humid_date;
+  var aqua_date = date + req.query.date;
+  var aqua_ec = req.query.ec;
+  var aqua_do = req.query.do;
+  var aqua_wtemp = req.query.wtemp;
+  var aqua_ph = req.query.ph;
+  connection.query('insert into aqua_fish (ph, do, wtemp, ec, date) values(?,?,?,?,?)',
+  [aqua_ph,aqua_do,aqua_ph,aqua_wtemp,aqua_ec, aqua_date],
+  (err, rows) => {
+      if(err) console.log(err);
+  });
+  res.send("ok");
+})
+
+//스마트팜 데이터 넣기
+app.get('/smart_humid_temp_A', (req, res) => {
+  var date = get_time();
+  humid_date = date  + req.query.date;
   humid = req.query.humid;
   temp = req.query.temp;
 
@@ -170,13 +101,29 @@ app.get('/smart_humid_temp_B', (req, res) => {
     console.log('humid : '+ humid);
     console.log('humid_date : '+ temp);
     console.log('humid_date : '+ humid_date);
-    connection.query('insert into smartfarm_humid_B (humid, date) values(?,?)',  
-    [humid, humid_date],
+    connection.query('insert into smartfarm_a (humid,temp,date) values(?,?,?)',  
+    [humid,temp, humid_date],
     (err, rows) => {
         if(err) console.log(err);
     })
-    connection.query('insert into smartfarm_temp_B (temp, date) values(?,?)',  
-    [temp, humid_date],
+    res.send("ok")
+  }
+});
+
+app.get('/smart_humid_temp_b', (req, res) => {
+  var date = get_time();
+  humid_date = date  + req.query.date;
+  humid = req.query.humid;
+  temp = req.query.temp;
+
+  if(humid && temp && humid_date){
+    console.log(req.query);
+    
+    console.log('humid : '+ humid);
+    console.log('humid_date : '+ temp);
+    console.log('humid_date : '+ humid_date);
+    connection.query('insert into smartfarm_b (humid,temp,date) values(?,?,?)',  
+    [humid,temp, humid_date],
     (err, rows) => {
         if(err) console.log(err);
     })
@@ -185,34 +132,6 @@ app.get('/smart_humid_temp_B', (req, res) => {
   else res.send("fail");
 });
 
-app.get('/smart_humid_temp', (req, res) => {
-  var date = get_time();
-  
-  humid_date = y+"-"+m+"-"+d+"-"+req.query.humid_date;
-  
-  humid = req.query.humid;
-  temp = req.query.temp;
-  
-  if(humid && temp && humid_date){
-    console.log(req.query);
-    
-    console.log('humid : '+ humid);
-    console.log('humid_date : '+ temp);
-    console.log('humid_date : '+ humid_date);
-    connection.query('insert into smartfarm_humid (humid, date) values(?,?)',  
-    [humid, humid_date],
-    (err, rows) => {
-        if(err) console.log(err);
-    })
-    connection.query('insert into smartfarm_temp (temp, date) values(?,?)',  
-    [temp, humid_date],
-    (err, rows) => {
-        if(err) console.log(err);
-    })
-    res.send("ok")
-  }
-  else res.send("fail");
-});
 
 
 /**
@@ -220,62 +139,38 @@ app.get('/smart_humid_temp', (req, res) => {
  * 받아온 데이터 보여줘
  */
 
-//스마트팜 대기온도확인
-app.get('/get_smarttemp', function(req,res){
-  connection.query('SELECT * FROM smartfarm_temp_a order by id desc limit 1',(err , rows)=>{
+//스마트팜a 대기온도확인
+app.get('/get_smart_a', function(req,res){
+  connection.query('SELECT * FROM smartfarm_a order by id desc limit 1',(err , rows)=>{
     res.send(rows)
   });
 })
-// 스마트팜 대기 온&습도
-app.get('/get_smarthumid', function(req,res){
-  connection.query('SELECT * FROM smartfarm_humid_a order by id desc limit 1',(err , rows)=>{
+
+app.get('/get_smart_b', function(req,res){
+  connection.query('SELECT * FROM smartfarm_b order by id desc limit 1',(err , rows)=>{
     res.send(rows)
   });
 })
-//스마트팜 대기습도확인
-app.get('/get_smarthumid', function(req,res){
-  connection.query('SELECT * FROM smartfarm_humid order by id desc limit 1',(err , rows)=>{
+
+app.get('/get_aqau_bio', function(req,res){
+  connection.query('SELECT * FROM aqua_bio order by id desc limit 1',(err, rows)=>{
     res.send(rows)
-  });
+  })
 })
-//스마트팜 토양습도확인
-app.get('/get_humid', function(req,res){
-  // var humid = "asdf"
-  connection.query('SELECT * FROM smartfarm_humid order by id desc limit 1',(err , rows)=>{
-    console.log('con?')
+
+app.get('/get_aqau_fish', function(req,res){
+  connection.query('SELECT * FROM aqua_fish order by id desc limit 1',(err, rows)=>{
     res.send(rows)
-  });
-  // console.log(humid);
-  // res.send(humid);
+  })
 })
 
 //접속테스트용
-app.get('/a', (req,res) => {
+app.get('/test', (req,res) => {
     console.log(req.query.id);
     console.log("접속온당");
     if(req.query.id) res.send("값 O")
     res.send("값 X");
-});
-
-//아쿠아포닉스 ph 데이터 저장
-app.get('/aqua_ph', (req,res) => {
-  console.log('test');
-  ph = req.query.ph;
-  ph_date = req.query.ph_date;
-  if(req.query.ph && req.query.ph_date){
-    console.log(req.query);
-    
-    console.log('ph : '+req.query.ph);
-    console.log('date : '+req.query.ph_date);
-    connection.query('insert into aqua_ph (ph, date) values(?,?)',  
-    [ph,ph_date],
-    (err, rows) => {
-        if(err) console.log(err);
-    })
-    res.send("ok")
-  }
-  else res.send("fail");
-});
+})
 
 
 app.listen(3000);
